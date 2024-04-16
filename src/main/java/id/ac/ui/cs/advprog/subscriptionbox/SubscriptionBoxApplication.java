aeab4db8fe142480206048fbe7af2fa82683ec41
tiffanyadisuryo <tiffanyadisuryo@gmail.com>
2024-04-17 01:20:43 +0700

    (seharusnya) initial commit part 2

---
+++ src/main/java/id/ac/ui/cs/advprog/subscriptionbox/SubscriptionBoxApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.subscriptionbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionBoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubscriptionBoxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/subscriptionbox/SubscriptionBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.subscriptionbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
