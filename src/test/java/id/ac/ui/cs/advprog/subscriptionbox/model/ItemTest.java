b9a70054cb68c7a5235d7fd6abbcbcdf6e699f87
tiffanyadisuryo <tiffanyadisuryo@gmail.com>
2024-04-17 10:46:06 +0700

    [REFACTOR] Refactoring test to fit the Builder Design Pattern

--- src/test/java/id/ac/ui/cs/advprog/subscriptionbox/model/ItemTest.java
+++ src/test/java/id/ac/ui/cs/advprog/subscriptionbox/model/ItemTest.java
@@ -4,28 +4,29 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-//class ItemTest {
-//    Item item;
-//    @BeforeEach
-//    void setUp(){
-//        this.item = new Item();
-//        this.item.setItemId("eb558e9f-1c39-460e-8860-71af6af63bd6");
-//        item.setItemName("Sampo cap Bambang");
-//        item.setItemQuantity(100);
-//    }
-//
-//    @Test
-//    void testGetItemId(){
-//        assertEquals("eb558e9f-1c39-460e-8860-71af6af63bd6", item.getItemId());
-//    }
-//
-//    @Test
-//    void testGetItemName(){
-//        assertEquals("Sampo cap Bambang", item.getItemName());
-//    }
-//
-//    @Test
-//    void testGetItemQuantity(){
-//        assertEquals(100, this.item.getItemQuantity());
-//    }
-//}
+class ItemTest {
+    Item.Builder itemBuilder;
+    @BeforeEach
+    void setUp(){
+        Item.Builder itemBuilder = new Item.Builder()
+                .id("eb558e9f-1c39-460e-8860-71af6af63bd6")
+                .name("Sunscreen cap Bambang")
+                .desc("Sunscreen yang ga lengket di kulit");
+        itemBuilder.build();
+    }
+
+    @Test
+    void testGetItemId(){
+        assertEquals("eb558e9f-1c39-460e-8860-71af6af63bd6", itemBuilder.id());
+    }
+
+    @Test
+    void testGetItemName(){
+        assertEquals("Sunscreen cap Bambang", itemBuilder.name());
+    }
+
+    @Test
+    void testGetItemDesc(){
+        assertEquals("Sunscreen yang ga lengket di kulit", itemBuilder.desc());
+    }
+}
