59a18e426ed67df714b7cb4cf1d9f4e1ebf8e680
tiffanyadisuryo <tiffanyadisuryo@gmail.com>
2024-04-17 09:13:25 +0700

    testing ci/cd

--- src/test/java/id/ac/ui/cs/advprog/subscriptionbox/model/ItemTest.java
+++ src/test/java/id/ac/ui/cs/advprog/subscriptionbox/model/ItemTest.java
@@ -4,28 +4,28 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-class ItemTest {
-    Item item;
-    @BeforeEach
-    void setUp(){
-        this.item = new Item();
-        this.item.setItemId("eb558e9f-1c39-460e-8860-71af6af63bd6");
-        item.setItemName("Sampo cap Bambang");
-        item.setItemQuantity(100);
-    }
-
-    @Test
-    void testGetItemId(){
-        assertEquals("eb558e9f-1c39-460e-8860-71af6af63bd6", item.getItemId());
-    }
-
-    @Test
-    void testGetItemName(){
-        assertEquals("Sampo cap Bambang", item.getItemName());
-    }
-
-    @Test
-    void testGetItemQuantity(){
-        assertEquals(100, this.item.getItemQuantity());
-    }
-}
+//class ItemTest {
+//    Item item;
+//    @BeforeEach
+//    void setUp(){
+//        this.item = new Item();
+//        this.item.setItemId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+//        item.setItemName("Sampo cap Bambang");
+//        item.setItemQuantity(100);
+//    }
+//
+//    @Test
+//    void testGetItemId(){
+//        assertEquals("eb558e9f-1c39-460e-8860-71af6af63bd6", item.getItemId());
+//    }
+//
+//    @Test
+//    void testGetItemName(){
+//        assertEquals("Sampo cap Bambang", item.getItemName());
+//    }
+//
+//    @Test
+//    void testGetItemQuantity(){
+//        assertEquals(100, this.item.getItemQuantity());
+//    }
+//}
