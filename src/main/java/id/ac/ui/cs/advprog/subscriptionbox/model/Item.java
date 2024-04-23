5b8e8a0ca04479e1cbba266f1cdf1240b40f5101
tiffanyadisuryo <tiffanyadisuryo@gmail.com>
2024-04-24 00:40:31 +0700

    implement description in Item class

--- src/main/java/id/ac/ui/cs/advprog/subscriptionbox/model/Item.java
+++ src/main/java/id/ac/ui/cs/advprog/subscriptionbox/model/Item.java
@@ -7,10 +7,15 @@ public class Item{
     private String id;
     private String name;
     private String image;
+    private String description;
 
     protected Item(ItemBuilder builder) {
         this.id = builder.getId();
         this.name = builder.getName();
         this.image = builder.getImage();
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
