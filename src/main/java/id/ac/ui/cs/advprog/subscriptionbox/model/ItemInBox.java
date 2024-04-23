075b295132faea6b6890bc50995691141f87d4b7
tiffanyadisuryo <tiffanyadisuryo@gmail.com>
2024-04-24 00:56:37 +0700

    [RED] added more test for description, itemInBoxList, and price to SubscriptionBoxTest

---
+++ src/main/java/id/ac/ui/cs/advprog/subscriptionbox/model/ItemInBox.java
@@ -0,0 +1,2 @@
+package id.ac.ui.cs.advprog.subscriptionbox.model;public class ItemInBox {
+}
--- src/test/java/id/ac/ui/cs/advprog/subscriptionbox/model/SubscriptionBoxTest.java
+++ src/test/java/id/ac/ui/cs/advprog/subscriptionbox/model/SubscriptionBoxTest.java
@@ -3,10 +3,13 @@ package id.ac.ui.cs.advprog.subscriptionbox.model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 class SubscriptionBoxTest {
     BoxBuilder boxBuilder;
     SubscriptionBox box;
+    ArrayList<ItemInBox> itemInBoxList = new ArrayList<>();
     @BeforeEach
     void setUp(){
         BoxBuilder boxBuilder = new BoxBuilder()
@@ -14,6 +17,16 @@ class SubscriptionBoxTest {
                 .name("Paket cap Bambang")
                 .image("link gambar box");
         box = boxBuilder.build();
+        box.setDescription("Paket lengkap");
+        box.setPrice(200000);
+        ItemBuilder itemBuilder = new ItemBuilder()
+                .id("eb558e9f-1c39-460e-8860-71af6af63bd6")
+                .name("Sunscreen cap Bambang")
+                .image("link gambar");
+        Item item = itemBuilder.build();
+        ItemInBox itemInBox = new ItemInBox(item, 2);
+        itemInBoxList.add(itemInBox);
+        box.setItemInBoxList(itemInBoxList);
     }
 
     @Test
@@ -29,5 +42,20 @@ class SubscriptionBoxTest {
     @Test
     void testGetBoxImage(){
         assertEquals("link gambar box", box.getImage());
+    }
+
+    @Test
+    void testGetBoxPrice(){
+        assertEquals(200000, box.getPrice());
+    }
+
+    @Test
+    void testGetItemInBoxList(){
+        assertSame(itemInBoxList, box.getItemInBoxList());
+    }
+
+    @Test
+    void testGetDescription(){
+        assertEquals("Paket lengkap", box.getDescription());
     }
 }
