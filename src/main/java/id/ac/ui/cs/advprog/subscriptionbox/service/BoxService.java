5d8d6d60e08d8717558ffb8082a0d1973d5b78b8
tiffanyadisuryo <tiffanyadisuryo@gmail.com>
2024-05-01 20:45:03 +0700

    [GREEN] IMPLEMENT BoxService to pass test

--- src/main/java/id/ac/ui/cs/advprog/subscriptionbox/service/BoxService.java
+++ src/main/java/id/ac/ui/cs/advprog/subscriptionbox/service/BoxService.java
@@ -1,2 +1,13 @@
-package id.ac.ui.cs.advprog.subscriptionbox.service;public interface BoxService {
+package id.ac.ui.cs.advprog.subscriptionbox.service;
+
+import id.ac.ui.cs.advprog.subscriptionbox.model.Item;
+import id.ac.ui.cs.advprog.subscriptionbox.model.SubscriptionBox;
+
+import java.util.List;
+public interface BoxService {
+    public SubscriptionBox create(SubscriptionBox box);
+    public List<SubscriptionBox> findAll();
+    SubscriptionBox findById(String boxId);
+    public SubscriptionBox update(String idBox, SubscriptionBox newBox);
+    public void deleteById(String idBox);
 }
--- src/main/java/id/ac/ui/cs/advprog/subscriptionbox/service/BoxServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/subscriptionbox/service/BoxServiceImpl.java
@@ -1,2 +1,64 @@
-package id.ac.ui.cs.advprog.subscriptionbox.service;public class BoxServiceImpl {
+package id.ac.ui.cs.advprog.subscriptionbox.service;
+
+import id.ac.ui.cs.advprog.subscriptionbox.model.Item;
+import id.ac.ui.cs.advprog.subscriptionbox.model.SubscriptionBox;
+import id.ac.ui.cs.advprog.subscriptionbox.repository.BoxManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BoxServiceImpl implements BoxService {
+    @Autowired
+    private BoxManager boxManager;
+
+    @Override
+    public SubscriptionBox create(SubscriptionBox box) {
+        boxManager.save(box);
+        return box;
+    }
+
+    @Override
+    public List<SubscriptionBox> findAll() {
+        return boxManager.findAll();
+    }
+
+    @Override
+    public SubscriptionBox findById(String id) {
+        Optional<SubscriptionBox> optionalBox = boxManager.findById(id);
+        if (optionalBox.isPresent()) {
+            return optionalBox.get();
+        } else {
+            // Handle the case where the item with the given ID is not found
+            throw new RuntimeException("Box not found with ID: " + id);
+        }
+    }
+
+    @Override
+    public SubscriptionBox update(String idBox, SubscriptionBox newBox){
+        Optional<SubscriptionBox> optionalBox = boxManager.findById(idBox);
+        if (optionalBox.isPresent()) {
+            SubscriptionBox existingBox= optionalBox.get();
+            // Update user fields
+            existingBox.setDescription(newBox.getName());
+            existingBox.setItemInBoxList(newBox.getItemInBoxList());
+            existingBox.setPrice(newBox.getPrice());
+            // Save and return updated user
+            boxManager.save(existingBox);
+            return existingBox;
+        } else {
+            // Handle user not found
+            throw new RuntimeException("Box not found with ID: " + idBox);
+        }
+    }
+
+    @Override
+    public void deleteById(String idBox){
+        boxManager.deleteById(idBox);
+    }
 }
+
+
