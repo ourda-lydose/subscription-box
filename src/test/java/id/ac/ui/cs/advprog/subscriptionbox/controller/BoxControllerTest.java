87ed5326e839a4781861f884c52b506e4874838f
tiffanyadisuryo <tiffanyadisuryo@gmail.com>
2024-05-01 20:46:06 +0700

    [RED] added test for controller

---
+++ src/test/java/id/ac/ui/cs/advprog/subscriptionbox/controller/BoxControllerTest.java
@@ -0,0 +1,194 @@
+package id.ac.ui.cs.advprog.subscriptionbox.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import id.ac.ui.cs.advprog.subscriptionbox.model.*;
+import id.ac.ui.cs.advprog.subscriptionbox.repository.BoxManager;
+import id.ac.ui.cs.advprog.subscriptionbox.service.BoxService;
+import id.ac.ui.cs.advprog.subscriptionbox.service.ItemService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(BoxController.class)
+public class BoxControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private BoxController boxController;
+
+    @MockBean
+    private BoxService boxService;
+
+    @MockBean
+    BoxManager boxManager;
+
+    @MockBean
+    private ItemService itemService;
+
+    private ObjectMapper objectMapper;
+
+    BoxBuilder boxBuilder;
+    SubscriptionBox box;
+    SubscriptionBox boxLain;
+
+    Set<ItemInBox> itemInBoxList = new HashSet<>();
+    Set<ItemInBox> itemInBoxList2 = new HashSet<>();
+
+    @BeforeEach
+    public void setup() {
+        objectMapper = new ObjectMapper();
+        mockMvc = MockMvcBuilders.standaloneSetup(boxController).build();
+        BoxBuilder boxBuilder = new BoxBuilder()
+                .id("eb558e9f-1c39-460e-8860-71af6af63bd8")
+                .name("Paket cap Bambang")
+                .image("link gambar box");
+        box = boxBuilder.build();
+        box.setDescription("Paket lengkap");
+        box.setPrice(200000);
+        ItemBuilder itemBuilder = new ItemBuilder()
+                .id("eb558e9f-1c39-460e-8860-71af6af63bd6")
+                .name("Sunscreen cap Bambang")
+                .image("link gambar");
+        Item item = itemBuilder.build();
+        ItemInBox itemInBox = new ItemInBox(item.getId(), 2);
+        itemInBoxList.add(itemInBox);
+        box.setItemInBoxList(itemInBoxList);
+
+        BoxBuilder boxBuilder2 = new BoxBuilder()
+                .id("eb558e9f-1c39-460e-8860-71af6af63bd9")
+                .name("Paket cap Bambang mini")
+                .image("link gambar box lageh");
+        boxLain = boxBuilder2.build();
+        boxLain.setDescription("Paket ga lengkap");
+        boxLain.setPrice(200);
+        ItemInBox itemInBox2 = new ItemInBox(item.getId(), 1);
+        itemInBoxList2.add(itemInBox2);
+        boxLain.setItemInBoxList(itemInBoxList2);
+    }
+
+    @Test
+    public void testCreateBox() throws Exception {
+        when(boxService.create(any(SubscriptionBox.class))).thenReturn(box);
+
+        mockMvc.perform(post("/box")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(box)))
+                .andExpect(status().isCreated())
+                .andExpect(content().json(objectMapper.writeValueAsString(box)));
+
+        verify(boxService, times(1)).create(any(SubscriptionBox.class));
+    }
+
+    @Test
+    public void testGetAllBoxes() throws Exception {
+        List<SubscriptionBox> allBoxes = new ArrayList<>();
+        allBoxes.add(box);
+
+        when(boxService.findAll()).thenReturn(allBoxes);
+
+        mockMvc.perform(get("/box"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(allBoxes)));
+
+        verify(boxService, times(1)).findAll();
+    }
+
+    @Test
+    public void testGetBoxById_Found() throws Exception {
+
+        when(boxService.findById(box.getId())).thenReturn(box);
+
+        mockMvc.perform(get("/box/" + box.getId()))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(box)));
+
+        verify(boxService, times(1)).findById(box.getId());
+    }
+
+    @Test
+    public void testGetBoxById_NotFound() throws Exception {
+
+        when(boxService.findById("lala")).thenReturn(null);
+
+        mockMvc.perform(get("/box/" + "lala"))
+                .andExpect(status().isNotFound());
+
+        verify(boxService, times(1)).findById("lala");
+    }
+
+    @Test
+    public void testGetItemsInBox_Found() throws Exception {
+
+        // Mock boxService.findById to return the box with items
+        when(boxService.findById(box.getId())).thenReturn(box);
+
+        mockMvc.perform(get("/box/" + box.getId() + "/items"))
+                .andExpect(status().isOk())  // Expect OK status for items found
+                .andExpect(jsonPath("$[0]").exists()); // Verify at least one item in response
+    }
+
+    @Test
+    public void testGetItemsInBox_NotFound() throws Exception {
+
+        // Mock boxService.findById to return the box without items
+        when(boxService.findById(box.getId())).thenReturn(box);
+
+        box.setItemInBoxList(null);
+
+        mockMvc.perform(get("/box/" + box.getId() + "/items"))
+                .andExpect(status().isOk()); // Expect OK status even if no items
+    }
+
+//    @Test
+//    public void testUpdateBox_Success() throws Exception {
+//        // Mock findById to return existing item
+//        when(boxService.findById(box.getId())).thenReturn(box);
+//        when(boxService.findById(boxLain.getId())).thenReturn(boxLain);
+//        when(boxService.update(box.getId(), boxLain)).thenReturn(boxLain);
+//
+//        // Update the item
+//        mockMvc.perform(put("/box/" + box.getId())
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .content(objectMapper.writeValueAsString(boxLain)))
+//                .andExpect(status().isOk())
+//                .andExpect(content().json(objectMapper.writeValueAsString(boxLain)));
+//
+//        verify(boxService, times(1)).update(box.getId(), boxLain);
+//    }
+
+    @Test
+    public void testUpdateBox_NotFound() throws Exception {
+        when(boxManager.findById("lalallalalala")).thenReturn(null);
+
+        mockMvc.perform(put("/box/" + "lalallalalala")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(box)))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void testDeleteBox_Success() throws Exception {
+
+        mockMvc.perform(delete("/box/" + box.getId()))
+                .andExpect(status().isNoContent()); // Expect NO_CONTENT
+
+        verify(boxService, times(1)).deleteById(box.getId());
+    }
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/subscriptionbox/controller/ItemControllerTest.java
@@ -0,0 +1,158 @@
+package id.ac.ui.cs.advprog.subscriptionbox.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper; // Needed for JSON parsing
+import id.ac.ui.cs.advprog.subscriptionbox.model.Item;
+import id.ac.ui.cs.advprog.subscriptionbox.model.ItemBuilder;
+import id.ac.ui.cs.advprog.subscriptionbox.repository.ItemManager;
+import id.ac.ui.cs.advprog.subscriptionbox.service.ItemService;
+import id.ac.ui.cs.advprog.subscriptionbox.service.ItemServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ItemController.class) // Annotate with WebMvcTest for web layer testing
+public class ItemControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ItemController itemController;
+
+    @MockBean // Mock the ItemService to avoid interacting with real database
+    private ItemServiceImpl itemService;
+
+    @MockBean
+    private ItemManager itemManager;
+
+    private ObjectMapper objectMapper;
+
+    Item item1;
+    Item item2;
+
+    @BeforeEach
+    public void setup() {
+        objectMapper = new ObjectMapper();
+        mockMvc = MockMvcBuilders.standaloneSetup(itemController).build();
+        ItemBuilder itemBuilder1 = new ItemBuilder()
+                .id("eb558e9f-1c39-460e-8860-71af6af63bd6")
+                .name("Sunscreen cap Bambang")
+                .image("link gambar");
+        item1 = itemBuilder1.build();
+        item1.setDescription("Sunscreen yang licin di kulit");
+
+        ItemBuilder itemBuilder2 = new ItemBuilder()
+                .id("eb558e9f-1c39-460e-8860-71af6af63bd7")
+                .name("Sunscreen cap Bambang uhuy")
+                .image("link gambar lageh");
+        item2 = itemBuilder2.build();
+        item2.setDescription("Sunscreen yang lembab di kulit");
+    }
+
+    @Test
+    public void testCreateItem() throws Exception {
+
+        when(itemService.create(any(Item.class))).thenReturn(item1);
+
+        mockMvc.perform(post("/item")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(item1)))
+                .andExpect(status().isCreated()) // Expect CREATED status code
+                .andExpect(content().json(objectMapper.writeValueAsString(item1)));
+
+        verify(itemService, times(1)).create(any(Item.class));
+    }
+
+    @Test
+    public void testGetAllItems() throws Exception {
+        List<Item> allItems = new ArrayList<>();
+        allItems.add(item1);
+        allItems.add(item2);
+
+        when(itemService.findAll()).thenReturn(allItems);
+
+        mockMvc.perform(get("/item"))
+                .andExpect(status().isOk()) // Expect OK status code
+                .andExpect(content().json(objectMapper.writeValueAsString(allItems)));
+
+        verify(itemService, times(1)).findAll();
+    }
+
+    @Test
+    public void testGetItemById_Found() throws Exception {
+
+        when(itemService.findById(item1.getId())).thenReturn(item1);
+
+        mockMvc.perform(get("/item/" + item1.getId()))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(item1)));
+
+        verify(itemService, times(1)).findById(item1.getId());
+    }
+
+    @Test
+    public void testGetItemById_NotFound() throws Exception {
+        String itemId = "456";
+
+        when(itemService.findById(itemId)).thenReturn(null);
+
+        mockMvc.perform(get("/item/" + itemId))
+                .andExpect(status().isNotFound()); // Expect NOT_FOUND status code
+
+        verify(itemService, times(1)).findById(itemId);
+    }
+
+    @Test
+    public void testUpdateItem_Success() throws Exception {
+
+        // Mock findById to return existing item
+        when(itemService.findById(item1.getId())).thenReturn(item1);
+        when(itemService.findById(item2.getId())).thenReturn(item2);
+        when(itemService.update(item1.getId(), item2)).thenReturn(item2);
+
+        // Update the item
+        mockMvc.perform(put("/item/" + "eb558e9f-1c39-460e-8860-71af6af63bd6")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(item2)))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(item2)));
+
+        verify(itemService, times(1)).update(item1.getId(), item2);
+    }
+
+    @Test
+    public void testUpdateItem_NotFound() throws Exception {
+        when(itemManager.findById("lalallalalala")).thenReturn(null);
+
+        mockMvc.perform(put("/item/" + "lalallalalala")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(item1)))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void testDeleteItem_Success() throws Exception {
+
+        mockMvc.perform(delete("/item/" + item1.getId()))
+                .andExpect(status().isNoContent()); // Expect NO_CONTENT
+
+        verify(itemService, times(1)).deleteById(item1.getId());
+    }
+
+
+}
